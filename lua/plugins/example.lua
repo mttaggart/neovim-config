948ab67c8379badfbd0ae7d8098ed977b5762c04
Michael Taggart <mtaggart@taggart-tech.com>
2023-11-06 21:51:23 -0800

    Pywright

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -216,7 +216,18 @@ return {
       servers = {
         -- tsserver will be automatically installed with mason and loaded with lspconfig
         tsserver = {},
-        pyright = {},
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "openFilesOnly",
+                useLibraryCodeForTypes = true,
+              },
+            },
+          },
+        },
+        hls = {},
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
